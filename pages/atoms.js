a623611192586ddd1e8302374f2ed18cf14a6c53
scottie-schneider <scott@laclave.co>
2019-10-09 18:15:39 -0500

    found 360 tour example

--- pages/atoms.js
+++ pages/atoms.js
@@ -131,7 +131,7 @@ const Atoms = props => (
         <iframe 
           width="342" 
           height="200" 
-          src="https://www.youtube.com/embed/_4czG6OLoCI" 
+          src="https://www.youtube.com/embed/NHcJ8zTnaG4" 
           frameborder="0" 
           allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
           allowfullscreen 
