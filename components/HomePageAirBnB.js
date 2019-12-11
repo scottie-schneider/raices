7d046abbbd6154a6635d4d304335ace97bd44dab
scottie-schneider <scott@laclave.co>
2019-12-11 13:29:45 -0500

    playing with width to fix mobile view of search container

--- components/HomePageAirBnB.js
+++ components/HomePageAirBnB.js
@@ -71,7 +71,7 @@ const HomePageStyle = styled.div`
     display: grid;
     grid-template-columns: 1fr;
     align-items: center;
-    width: 90vw;    
+    width: 90%;    
     min-width: 200px;
     margin: 0 auto;
   }
