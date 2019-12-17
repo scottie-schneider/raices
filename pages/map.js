25c39b8ecea09ee9c6ff4176ad648b6b7c057b49
scottie-schneider <scott@laclave.co>
2019-12-17 15:07:14 -0500

    maps update

--- pages/map.js
+++ pages/map.js
@@ -3,8 +3,7 @@ import WithMarkers from '../components/WithMarkers';
 class Map extends React.Component {
   render() {
     return (
-      // <WithMarkers />
-      <p>lolz</p>
+      <WithMarkers />      
     )
   }
 }
