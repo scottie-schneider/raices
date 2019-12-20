9fd740d8b31b4b0da5d9a445c3eaec0e3e57136a
scottie-schneider <scott@laclave.co>
2019-12-19 20:45:32 -0500

    revert to raices

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,9 @@
 import HomePageAirBnB from '../components/HomePageAirBnB';
-import Items from '../components/Items';
 
 class Home extends React.Component {
   render() {
     return (
-      // <HomePageAirBnB />
-      <Items page={parseFloat(this.props.query.page) || 1} />
+      <HomePageAirBnB />      
     )
   }
 }
